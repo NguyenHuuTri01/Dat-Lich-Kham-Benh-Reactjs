aac0ec43f82f47f0a0180275465c6eb8270da7d6
Nguyen Huu Tri <nguyenhuutri0905@gmail.com>
2022-06-17 10:12:16 +0700

    Update 17/06/2022

--- NodejsTest/src/controllers/userController.js
+++ NodejsTest/src/controllers/userController.js
@@ -20,7 +20,25 @@ let handleLogin = async (req, res) => {
     user: userData.user ? userData.user : {},
   });
 };
+let handleGetAllUsers = async (req, res) => {
+  let id = req.body.id;
+  if (!id) {
+    return res.status(200).json({
+      errCode: 1,
+      errMessage: "Missing required parameters",
+      users: [],
+    });
+  }
+
+  let users = await userService.getAllUsers(id);
+  return res.status(200).json({
+    errCode: 0,
+    errMessage: "OK",
+    users,
+  });
+};
 
 module.exports = {
   handleLogin: handleLogin,
+  handleGetAllUsers: handleGetAllUsers,
 };
