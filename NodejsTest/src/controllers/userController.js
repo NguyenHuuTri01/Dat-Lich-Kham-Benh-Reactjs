44238b142c82f3a9efcc7198608772e714ddb490
Nguyen Huu Tri <nguyenhuutri0905@gmail.com>
2022-06-23 16:04:14 +0700

    req.body=req.query

--- NodejsTest/src/controllers/userController.js
+++ NodejsTest/src/controllers/userController.js
@@ -21,7 +21,7 @@ let handleLogin = async (req, res) => {
   });
 };
 let handleGetAllUsers = async (req, res) => {
-  let id = req.body.id;
+  let id = req.query.id;
   if (!id) {
     return res.status(200).json({
       errCode: 1,
