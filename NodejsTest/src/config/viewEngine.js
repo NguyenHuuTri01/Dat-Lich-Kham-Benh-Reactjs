96e0f8d19692f497db75b9f5e0dee35908ea589a
NguyenHuuTri01 <104763264+NguyenHuuTri01@users.noreply.github.com>
2022-06-26 09:36:12 +0700

    Delete NodejsTest directory

--- NodejsTest/src/config/viewEngine.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-let configviewEngine = (app) => {
-  app.use(express.static("./src/public"));
-  app.set("view engine", "ejs");
-  app.set("views", "./src/views");
-};
-
-module.exports = configviewEngine;
