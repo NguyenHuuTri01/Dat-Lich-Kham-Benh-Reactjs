abdd3cd7270fe4240076055ced932bac5e867d2f
Nguyen Huu Tri <nguyenhuutri0905@gmail.com>
2022-06-26 09:33:40 +0700

    rename

---
+++ backend-nodejs/src/route/web.js
@@ -0,0 +1,26 @@
+import express from "express";
+import homeController from "../controllers/homeControllerr";
+import userController from "../controllers/userController";
+
+let router = express.Router();
+
+let initWebRoutes = (app) => {
+  router.get("/", homeController.getHomePage);
+  router.get("/crud", homeController.getCRUD);
+  router.post("/post-crud", homeController.postCRUD);
+  router.get("/get-crud", homeController.displayGetCRUD);
+  router.get("/edit-crud", homeController.getEditCRUD);
+
+  router.post("/put-crud", homeController.putCRUD);
+  router.get("/delete-crud", homeController.deleteCRUD);
+
+  router.post("/api/login", userController.handleLogin);
+  router.get("/api/get-all-users", userController.handleGetAllUsers);
+  router.post("/api/create-new-user", userController.handleCreateNewUser);
+  router.put("/api/edit-user", userController.handleEditUser);
+  router.delete("/api/delete-user", userController.handleDelelteUser);
+
+  return app.use("/", router);
+};
+
+module.exports = initWebRoutes;
