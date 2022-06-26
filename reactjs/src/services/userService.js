7c72790bddd01005862ef9af4c1be4c7c1f22bf0
NguyenHuuTri01 <104763264+NguyenHuuTri01@users.noreply.github.com>
2022-06-26 09:36:34 +0700

    Delete reactjs directory

--- reactjs/src/services/userService.js
+++
@@ -1,9 +0,0 @@
-import axios from "../axios";
-
-const handleLoginApi = (email, password) => {
-  return axios.post("api/login", { email, password });
-};
-const getAllUsers = (inputId) => {
-  return axios.get(`/api/get-all-users?id=${inputId}`);
-};
-export { handleLoginApi, getAllUsers };
