ec39abc7aea14c56321f0f5f1e17e729bcfeebe2
Nguyen Huu Tri <nguyenhuutri0905@gmail.com>
2022-06-28 09:31:03 +0700

    delete user

--- frontend-reactjs/src/containers/System/ModalUser.js
+++ frontend-reactjs/src/containers/System/ModalUser.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { FormattedMessage } from "react-intl";
 import { connect } from "react-redux";
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import { emitter } from "../../utils/emitter";
 
 class ModalUser extends Component {
   constructor(props) {
@@ -13,6 +14,18 @@ class ModalUser extends Component {
       lastName: "",
       address: "",
     };
+    this.listenToEmitter();
+  }
+  listenToEmitter() {
+    emitter.on("EVENT_CLEAR_MODAL_DATA", () => {
+      this.setState({
+        email: "",
+        password: "",
+        firstName: "",
+        lastName: "",
+        address: "",
+      });
+    });
   }
 
   componentDidMount() {}
