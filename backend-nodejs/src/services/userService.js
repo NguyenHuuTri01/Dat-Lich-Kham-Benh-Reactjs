760cb9adb410005f3e34442a6d2105bf255168d2
Nguyen Huu Tri <nguyenhuutri0905@gmail.com>
2022-07-23 15:29:22 +0700

    edit api update

--- backend-nodejs/src/services/userService.js
+++ backend-nodejs/src/services/userService.js
@@ -152,7 +152,7 @@ let deleteUser = (userId) => {
 let updateUser = (data) => {
   return new Promise(async (resolve, reject) => {
     try {
-      if (!data.id) {
+      if (!data.id || !data.roleId || !data.positionId || !data.gender) {
         resolve({
           errCode: 2,
           errMessage: "Missing require parameters",
@@ -166,6 +166,10 @@ let updateUser = (data) => {
         user.firstName = data.firstName;
         user.lastName = data.lastName;
         user.address = data.address;
+        user.roleId = data.roleId;
+        user.positionId = data.positionId;
+        user.gender = data.gender;
+        user.phonenumber = data.phonenumber;
         await user.save();
 
         resolve({
