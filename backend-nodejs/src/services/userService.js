a12495cd37201981d11a21d253c0c28bf7a7551c
Nguyen Huu Tri <nguyenhuutri0905@gmail.com>
2022-07-24 10:52:29 +0700

    update image

--- backend-nodejs/src/services/userService.js
+++ backend-nodejs/src/services/userService.js
@@ -116,6 +116,7 @@ let createNewUser = (data) => {
           gender: data.gender,
           roleId: data.roleId,
           positionId: data.positionId,
+          image: data.avatar,
         });
         resolve({
           errCode: 0,
@@ -170,6 +171,10 @@ let updateUser = (data) => {
         user.positionId = data.positionId;
         user.gender = data.gender;
         user.phonenumber = data.phonenumber;
+        if (data.avatar) {
+          user.image = data.avatar;
+        }
+
         await user.save();
 
         resolve({
