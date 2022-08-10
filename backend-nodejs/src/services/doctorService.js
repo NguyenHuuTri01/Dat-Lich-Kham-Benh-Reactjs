8de10807e9c60cf9dfa103ed0c9d4953b84afb25
Nguyen Huu Tri <nguyenhuutri0905@gmail.com>
2022-08-10 10:44:58 +0700

    api create schedule

--- backend-nodejs/src/services/doctorService.js
+++ backend-nodejs/src/services/doctorService.js
@@ -1,4 +1,8 @@
 import db from "../models/index";
+require('dotenv').config();
+import _ from "lodash";
+
+const MAX_NUMBER_SCHEDULE = process.env.MAX_NUMBER_SCHEDULE;
 
 let getTopDoctorHome = (limit) => {
   return new Promise(async (resolve, reject) => {
@@ -137,9 +141,57 @@ let getDetailDoctorById = (inputId) => {
     }
   })
 }
+let bulkCreateSchedule = (data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (!data.arrSchedule || !data.doctorId || !data.formatedDate) {
+        resolve({
+          errCode: 1,
+          errMessage: 'Missing required parameter!'
+        })
+      } else {
+        let schedule = data.arrSchedule;
+        if (schedule && schedule.length > 0) {
+          schedule = schedule.map(item => {
+            item.maxNumber = MAX_NUMBER_SCHEDULE;
+            return item;
+          })
+        }
+        // get all existing data
+        let existing = await db.Schedule.findAll({
+          where: { doctorId: data.doctorId, date: data.formatedDate },
+          attributes: ['timeType', 'date', 'doctorId', 'maxNumber'],
+          raw: true
+        });
+        // convert date
+        if (existing && existing.length > 0) {
+          existing = existing.map(item => {
+            item.date = new Date(item.date).getTime();
+            return item;
+          })
+        }
+        //compare different
+        let toCreate = _.differenceWith(schedule, existing, (a, b) => {
+          return a.timeType === b.timeType && a.date === b.date;
+        });
+        // create data
+        if (toCreate && toCreate.length > 0) {
+          await db.Schedule.bulkCreate(toCreate)
+        }
+        resolve({
+          errCode: 0,
+          errMessage: "ok"
+        })
+      }
+    } catch (e) {
+      reject(e);
+    }
+  })
+}
 module.exports = {
   getTopDoctorHome: getTopDoctorHome,
   getAllDoctors: getAllDoctors,
   saveDetailInforDoctor: saveDetailInforDoctor,
   getDetailDoctorById: getDetailDoctorById,
+  bulkCreateSchedule: bulkCreateSchedule
 };
